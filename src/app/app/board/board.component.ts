c6a3f952a106ffb0d0101ef9396dba922adcc4a6
Rob Monhemius <robmonhemius@gmail.com>
2023-04-24 03:27:34 +0200

    implemented generic hexagon layout

--- src/app/app/board/board.component.ts
+++ src/app/app/board/board.component.ts
@@ -7,26 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  public cells: unknown[] = [
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {}
-  ];
-
-  constructor() { }
+  public board = {
+    rows: 10,
+    columns: 8
+  }
+  public cells: any[] = [];
 
   ngOnInit(): void {
+    this.cells = Array(this.board.rows * this.board.columns)
+      .fill(null)
+      .map( (_, index) => ({
+      column: index % this.board.columns,
+      row: Math.floor( index / this.board.columns ) + 1
+    }) );
   }
 
 }
