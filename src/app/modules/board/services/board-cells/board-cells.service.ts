55b440185abd450fddef94b0404aede8294f7a26
Rob Monhemius <robmonhemius@gmail.com>
2023-05-03 16:06:04 +0200

    refactor: moved board-cell generation responsibility

--- src/app/modules/board/services/board-cells/board-cells.service.ts
+++ src/app/modules/board/services/board-cells/board-cells.service.ts
@@ -1,30 +1,23 @@
 import { Injectable } from '@angular/core';
 import BoardCell from '../../data/BoardCell';
+import { BoardCellService } from './board-cell/board-cell.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoardCellsService {
 
-  // may belong in a BoardCells service
-  private default_record(): BoardCell { 
-    return {
-      column: 0,
-      row: 0,
-      id: 0
-    }
-  }
-
-  constructor() { }
+  constructor(
+    private board_cell_service: BoardCellService
+  ) { }
 
   public generate_records( rows: number, columns: number ): BoardCell[] {
-    return Array(rows * columns)
-      .fill( this.default_record() )
-      .map( (_, index) => ({
-        column: index % columns + 1, 
-        row: Math.floor( index / columns ) + 1,
-        id: index
-      }) );
+    return Array(rows * columns).fill( null )
+      .map( (_, index) => { 
+        const column = index % columns + 1;
+        const row = Math.floor( index / columns ) + 1;
+        return this.board_cell_service.create( column, row, index );
+      } );
   }
 
 
