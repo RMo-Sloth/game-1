1a4f3f839552060d40675bee86e1e1c451039e8f
Rob Monhemius <robmonhemius@gmail.com>
2023-05-03 15:47:52 +0200

    initial create method implementation

--- src/app/modules/board/services/board-cells/board-cell/board-cell.service.spec.ts
+++ src/app/modules/board/services/board-cells/board-cell/board-cell.service.spec.ts
@@ -13,4 +13,33 @@ describe('BoardCellService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe( 'create(column, row, id)', () => {
+
+    it('should have the correct id property', () => {
+      const result = service.create( 1,1,0 );
+      const result_2 = service.create( 8,9,2 );
+
+      expect( result.id ).toBe( 0 );
+      expect( result_2.id ).toBe( 2 );
+    });
+
+    it('should have the correct column property', () => {
+      const result = service.create( 1,1,0 );
+      const result_2 = service.create( 8,9,2 );
+
+      expect( result.column ).toBe( 1 );
+      expect( result_2.column ).toBe( 8 );
+    });
+
+    it('should have the correct row property', () => {
+      const result = service.create( 1,1,0 );
+      const result_2 = service.create( 8,9,2 );
+
+      expect( result.row ).toBe( 1 );
+      expect( result_2.row ).toBe( 9 );
+    });
+
+  });
+
 });
--- src/app/modules/board/services/board-cells/board-cell/board-cell.service.ts
+++ src/app/modules/board/services/board-cells/board-cell/board-cell.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import BoardCell from '../../../data/BoardCell';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoardCellService {
 
-  constructor() { }
+  public create( column: number, row: number, id: number ): BoardCell {
+    return { column, row, id };
+  }
+
 }
