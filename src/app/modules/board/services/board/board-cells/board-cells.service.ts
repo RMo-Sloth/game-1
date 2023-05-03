81530ab62961e9ebf23ce35f4177c916f43a7208
Rob Monhemius <robmonhemius@gmail.com>
2023-05-03 21:34:24 +0200

    refactor: moved cell creation

--- src/app/modules/board/services/board/board-cells/board-cells.service.ts
+++ src/app/modules/board/services/board/board-cells/board-cells.service.ts
@@ -14,9 +14,7 @@ export class BoardCellsService {
   public generate_records( rows: number, columns: number ): BoardCell[] {
     return Array(rows * columns).fill( null )
       .map( (_, index) => { 
-        const column = index % columns + 1;
-        const row = Math.floor( index / columns ) + 1;
-        return this.board_cell_service.create( column, row, index );
+        return this.board_cell_service.create( columns, rows, index );
       } );
   }
 
