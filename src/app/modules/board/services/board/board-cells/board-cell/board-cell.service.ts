74beaf3a9809f4e357b665619e0b1f8cd0d2c3ad
Rob Monhemius <robmonhemius@gmail.com>
2023-05-05 01:12:25 +0200

    refactor: naming and renaming

--- src/app/modules/board/services/board/board-cells/board-cell/board-cell.service.ts
+++ src/app/modules/board/services/board/board-cells/board-cell/board-cell.service.ts
@@ -7,40 +7,41 @@ import BoardCell from 'src/app/modules/board/services/board/board-cells/board-ce
 export class BoardCellService {
 
   public create( columns: number, rows: number, id: number ): BoardCell {
-    const column = ( Math.floor( id / columns ) % 2 === 0) ? id % columns + 1 : id % columns + 1 + columns;
+    const column = (( Math.floor( id / columns ) % 2 === 0) ? id % columns : id % columns + columns) + 1;
     const row = Math.floor( id / (columns * 2) ) + 1;
     
     const neighbours: number[] = [];
 
-    // top position
-    const position_1 = id - 2 * columns;
-    if( row > 1 )
-      neighbours.push( position_1 );
+    const top_position = id - 2 * columns;
+    if( this.is_existing_cell( top_position, rows, columns) )
+      neighbours.push( top_position );
 
-    // top-right position
-    const position_2 = id + (column % 2 ? -rows * 2 + 3: 1);
-    if( row > 1 && column < columns * 2|| row === 1 && column % 2 === 0 && column < columns * 2 )
-      neighbours.push( position_2 );
+    const top_right_position = id + (column % 2 ? -rows * 2 + 3: 1);
+    if( this.is_existing_cell( top_right_position, rows, columns) ) // also need to check not to add to last column
+      neighbours.push( top_right_position );
 
-    // const position_3 = id + column;
-    // if( position_3 < column*row )
+    // bottom-right position
+    // const position_3 = id + 2 * columns + 1;
+    // if( !this.is_in_last_row( row, rows ) )
     //   neighbours.push( position_3 );
     
-    // bottom position
-    const position_4 = id + 2 * columns;
-    if( row < rows / 2 )
-      neighbours.push( position_4 );
+    const bottom_position = id + 2 * columns;
+    if( this.is_existing_cell( bottom_position, rows, columns) )
+      neighbours.push( bottom_position );
     
     // const position_5 = id + column - 1;
     // if( position_5 < column*row && position_5 > 0 )
     //   neighbours.push( position_5 );
 
-    // top-left position
-    const position_6 = id + (column % 2 ? -rows * 2 + 1: -1);
-    if( row > 1 && column > 1 || row === 1 && column % 2 === 0 )
-      neighbours.push( position_6 );
+    const top_left_position = id + (column % 2 ? -rows * 2 + 1: -1);
+    if( this.is_existing_cell( top_left_position, rows, columns) ) // also need to check not to add to first column
+      neighbours.push( top_left_position );
 
     return { column, row, id, neighbours };
+  }
+
+  private is_existing_cell( index: number, rows: number, columns: number ): boolean {
+    return index >= 0 && index < columns * rows;
   }
 
 }
