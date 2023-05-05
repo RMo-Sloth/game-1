c45ada17d75d582c9c32efa204f5df44b37a35f0
Rob Monhemius <robmonhemius@gmail.com>
2023-05-05 02:40:14 +0200

    refactor: extract method

--- src/app/modules/board/services/board/board-cells/board-cell/board-cell.service.ts
+++ src/app/modules/board/services/board/board-cells/board-cell/board-cell.service.ts
@@ -9,34 +9,39 @@ export class BoardCellService {
   public create( columns: number, rows: number, id: number ): BoardCell {
     const column = (( Math.floor( id / columns ) % 2 === 0) ? id % columns : id % columns + columns) + 1;
     const row = Math.floor( id / (columns * 2) ) + 1;
-    
-    const neighbours: number[] = [];
-
-    const top_position = id - 2 * columns;
-    if( this.is_existing_cell( top_position, rows, columns) )
-      neighbours.push( top_position );
-
-    const top_right_position = id + (column % 2 ? -rows * 2 + 3: 1);
-    if( this.is_existing_cell( top_right_position, rows, columns) && !this.is_right_column( column, columns ) )
-      neighbours.push( top_right_position );
-
-    const bottom_right_position = column % 2 === 0 ? id + 1 + 2 * columns : id + 1;
-    if( this.is_existing_cell( bottom_right_position, rows, columns) && !this.is_right_column( column, columns ) )
-      neighbours.push( bottom_right_position );
-    
-    const bottom_position = id + 2 * columns;
-    if( this.is_existing_cell( bottom_position, rows, columns) )
-      neighbours.push( bottom_position );
-    
-    const bottom_left_position = column % 2 === 0 ? id + 2 * columns - 1 : id - 1;
-    if( this.is_existing_cell( bottom_left_position, rows, columns) && column !== 1 )
-      neighbours.push( bottom_left_position );
-
-    const top_left_position = id + (column % 2 ? -rows * 2 + 1: -1);
-    if( this.is_existing_cell( top_left_position, rows, columns) && column !== 1 )
-      neighbours.push( top_left_position );
+    const neighbours: number[] = this.compute_neighbours(id, columns, rows, column);
 
     return { column, row, id, neighbours };
+  }
+
+  private compute_neighbours(id: number, columns: number, rows: number, column: number) {
+    const neighbours = [];
+
+    const top_position = id - 2 * columns;
+    if (this.is_existing_cell(top_position, rows, columns))
+      neighbours.push(top_position);
+
+    const top_right_position = id + (column % 2 ? -rows * 2 + 3 : 1);
+    if (this.is_existing_cell(top_right_position, rows, columns) && !this.is_right_column(column, columns))
+      neighbours.push(top_right_position);
+
+    const bottom_right_position = column % 2 === 0 ? id + 1 + 2 * columns : id + 1;
+    if (this.is_existing_cell(bottom_right_position, rows, columns) && !this.is_right_column(column, columns))
+      neighbours.push(bottom_right_position);
+
+    const bottom_position = id + 2 * columns;
+    if (this.is_existing_cell(bottom_position, rows, columns))
+      neighbours.push(bottom_position);
+
+    const bottom_left_position = column % 2 === 0 ? id + 2 * columns - 1 : id - 1;
+    if (this.is_existing_cell(bottom_left_position, rows, columns) && column !== 1)
+      neighbours.push(bottom_left_position);
+
+    const top_left_position = id + (column % 2 ? -rows * 2 + 1 : -1);
+    if (this.is_existing_cell(top_left_position, rows, columns) && column !== 1)
+      neighbours.push(top_left_position);
+
+    return neighbours;
   }
 
   private is_existing_cell( index: number, rows: number, columns: number ): boolean {
