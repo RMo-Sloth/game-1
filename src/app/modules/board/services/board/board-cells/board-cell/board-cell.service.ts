3591c327f13bce3a50153efadd46cac85bfc1d89
Rob Monhemius <robmonhemius@gmail.com>
2023-05-05 04:54:46 +0200

    refactor: made the logic as simple as possible

--- src/app/modules/board/services/board/board-cells/board-cell/board-cell.service.ts
+++ src/app/modules/board/services/board/board-cells/board-cell/board-cell.service.ts
@@ -7,93 +7,45 @@ import BoardCell from 'src/app/modules/board/services/board/board-cells/board-ce
 export class BoardCellService {
 
   public create( columns: number, rows: number, id: number ): BoardCell {
-    const column = (( Math.floor( id / columns ) % 2 === 0) ? id % columns : id % columns + columns) + 1;
+    // NOTICE: I am merging 2 columns in 1 row to accommodate the hex grid in a somewhat comprehensible way
+    // EXTRA NOTE: Maybe it's even better to use 3 axis ( top-to-bottom, top-left to bottom-right and top-right to bottom-left ) instead of 2 ( column and row )
+    // That would make the neighbour calculations much easier, but slightly weird to define a grid-size
+    const column = (( Math.floor( id / columns ) % 2 === 0) ? 0 : columns) + id % columns + 1;
     const row = Math.floor( id / (columns * 2) ) + 1;
     const neighbours: number[] = this.compute_neighbours(id, columns, rows, column);
 
     return { column, row, id, neighbours };
   }
 
-  private compute_neighbours(id: number, columns: number, rows: number, column: number) {
-    // the plan
-    // x refactor to common column % 2 === 0  ternary
-    // x TEST
-    // x split into 2 paths ( odd and even )
-    // x TEST
-    // x remove irrelevant ternary code from both paths
-    // x TEST
-    // refactor to chains to calculate values ( arr.push().push() )
-    // temporary provide and filter out undefined
-    // TEST
-    // remove ternary conditions into the filter
-    // ??? the odd path excludes values to the 'first column' 
-    // ??? TEST
-    // ??? the even path excludes values to the 'last column' 
-    // ?? TEST
+  private compute_neighbours(id: number, columns: number, rows: number, column: number): number[] {
+    const top_position = id - 2 * columns;
+    const bottom_position = id + 2 * columns;
+    let result = [ top_position, bottom_position ];
 
     if( column % 2 === 0 ) {
-      const neighbours = [];
-      const top_position = id - 2 * columns;
-      if (this.is_existing_cell(top_position, rows, columns))
-        neighbours.push(top_position);
-  
-      const bottom_position = id + 2 * columns;
-      if (this.is_existing_cell(bottom_position, rows, columns))
-        neighbours.push(bottom_position);
-  
-      const top_right_position = 1 + id;
-      if (this.is_existing_cell(top_right_position, rows, columns) && !this.is_right_column(column, columns))
-        neighbours.push(top_right_position);
-  
-      const bottom_right_position = id + 1 + 2 * columns;
-      if (this.is_existing_cell(bottom_right_position, rows, columns) && !this.is_right_column(column, columns))
-        neighbours.push(bottom_right_position);
-  
       const bottom_left_position = id + 2 * columns - 1;
-      if (this.is_existing_cell(bottom_left_position, rows, columns) && column !== 1)
-        neighbours.push(bottom_left_position);
-  
-      const top_left_position = -1 + id;
-      if (this.is_existing_cell(top_left_position, rows, columns) && column !== 1)
-        neighbours.push(top_left_position);
-      
-      return neighbours;
-
-    } else {
-
-      const neighbours = [];
-      const top_position = id - 2 * columns;
-      if (this.is_existing_cell(top_position, rows, columns))
-        neighbours.push(top_position);
-  
-      const bottom_position = id + 2 * columns;
-      if (this.is_existing_cell(bottom_position, rows, columns))
-        neighbours.push(bottom_position);
-  
-      const top_right_position = -rows * 2 + id + 3;
-      if (this.is_existing_cell(top_right_position, rows, columns) && !this.is_right_column(column, columns))
-        neighbours.push(top_right_position);
-  
-      const bottom_right_position = id + 1;
-      if (this.is_existing_cell(bottom_right_position, rows, columns) && !this.is_right_column(column, columns))
-        neighbours.push(bottom_right_position);
-  
+      const top_left_position = id - 1;
+      result = [ ...result, bottom_left_position, top_left_position ];
+    } 
+    else if( column !== 1 ) {
       const bottom_left_position = id - 1;
-      if (this.is_existing_cell(bottom_left_position, rows, columns) && column !== 1)
-        neighbours.push(bottom_left_position);
-  
-      const top_left_position = -rows * 2 + id + 1;
-      if (this.is_existing_cell(top_left_position, rows, columns) && column !== 1)
-        neighbours.push(top_left_position);
-      
-      return neighbours;
-
+      const top_left_position = id + 1 - 2 * rows;
+      result = [ ...result, bottom_left_position, top_left_position ];
     }
+    
+    if( column % 2 === 1 ) {
+      const top_right_position = id + 3 - 2 * rows;
+      const bottom_right_position = id + 1;
+      result = [ ...result, top_right_position, bottom_right_position ];
+    } 
+    else if( !this.is_right_column(column, columns) ) {
+      const top_right_position = id + 1;
+      const bottom_right_position = id + 1 + 2 * columns;
+      result = [ ...result, top_right_position, bottom_right_position ];
+    } 
 
-  }
+    return result.filter( val => val >= 0 && val < columns * rows );
 
-  private is_existing_cell( index: number, rows: number, columns: number ): boolean {
-    return index >= 0 && index < columns * rows;
   }
 
   private is_right_column( column: number, columns: number ): boolean {
